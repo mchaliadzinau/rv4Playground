776c76f9a3e1780b5f8fe99f052987e4f8e75a59
Rv4Flyver <Rvach.Flyver@gmail.com>
2017-11-22 17:27:21 +0300

    Implemented simplest template engine prototype

--- src/app/root.js
+++ src/app/root.js
@@ -10,7 +10,42 @@ console.log("root is LOADED");
 
 rv4 = {
     q: null,
-    w: null
+    w: null,
+    init: (window,jqlite)=>{
+        rv4.w = window;
+        rv4.q = jqlite;
+        initAppTree();
+    },
+    data: {
+        'app1': {
+            problems: [
+                "<td>1</td><td>2</td><td>3</td><td>4</td>",
+                "<td>5</td><td>6</td><td>7</td><td>8</td>"
+            ]
+        }
+    }
+}
+
+function initAppTree() {
+    let rv4s = rv4.q('*[rv4]'); // root nodes;
+    // LOOP THROUGH ROOT NODES
+    rv4s.forEach(root=>{
+        let rootName = root.attributes['rv4'].value; // ROOT NODE NAME
+        // LOOP THROUGH FOR NODES
+        let rv4Fors = rv4.q('*[rv4For]',root);
+        rv4Fors.forEach(f=>{
+            let node = rv4.q(f);
+            let dataName = f.attributes['rv4For'].value;
+            let data = rv4.data[rootName][dataName];
+            data.forEach(i=>{
+                let newNode = node.clone();
+                newNode.html(i);
+                node.parent().append(newNode)
+            })
+        })
+        // console.log(rv4Fors)
+    });
+    console.log(rv4s)
 }
 
 console.log("root EXECUTION is COMPLETED");
